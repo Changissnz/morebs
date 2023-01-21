af6e86295e5c76316d1eb3c864cb2c692faf4413
Richard Pham <phamrichard45@gmail.com>
2023-01-20 17:01:42 -0800

    add class<PolyFactorEst>

---
+++ morebs2/numerical_extras.py
@@ -0,0 +1,39 @@
+'''
+methods on integers such as multiple-finding
+'''
+from .poly_struct import * 
+
+def all_multiples(i):
+    l = abs(int(i / 2))
+    m = set()
+    for j in range(1,l + 1):
+        if j in m: break
+        if not i % j:
+            m |= set([j,int(i/j)])
+    return m
+
+def all_multiple_pairs(i):
+    l = abs(int(i / 2))
+    m = []
+    s = set()
+    for j in range(1,l + 1):
+        if not i % j:
+            if j in s: break
+            if int(i/j) in s: break
+            m.append((j,int(i/j)))
+            m.append((int(i/j),j))
+            s |= set([j,int(i/j)])
+
+            # case: negative
+            
+            if i < 0:
+                s |= set([-j,-int(i/j)])
+                m.append((-j,-int(i/j)))
+                m.append((-int(i/j),-j))            
+    return m
+
+
+def is_rational(f):
+    assert f >= -1. and f <= 1., "invalid float"
+    return "TODO"
+
