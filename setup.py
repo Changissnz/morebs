2bd3d0a22ce054e9b3afeddeeff1434b1b1fba86
Richard Pham <phamrichard45@gmail.com>
2024-04-06 15:44:39 -0400

    fixed conf.py and setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='morebs2',
-    version='0.0.7',
+    version='0.0.8',
     description='data structures to aid in numerical data generation and clustering',
     long_description=readme,
     author='Richard Pham',
