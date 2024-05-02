ab7f700cd1aa668e02b38cac8cf616932f4887a2
Richard Pham <phamrichard45@gmail.com>
2024-05-02 18:57:38 -0400

    increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='morebs2',
-    version='0.0.9',
+    version='0.0.10',
     description='data structures to aid in numerical data generation and clustering',
     long_description=readme,
     author='Richard Pham',
