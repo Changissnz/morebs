a9944894b736f861d0ef9c27007258efd930964a
Richard Pham <phamrichard45@gmail.com>
2025-06-22 17:09:35 -0400

    add method<float_to_string>

--- conf.py
+++ conf.py
@@ -9,7 +9,7 @@
 project = 'morebs'
 copyright = '2023, Richard Pham'
 author = 'Richard Pham'
-release = '0.1.18'
+release = '0.1.19'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
