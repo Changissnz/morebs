3f7128b38ed14c3efdc122f2c383dcdafa7ff171
Richard Pham <phamrichard45@gmail.com>
2025-07-31 04:30:41 -0400

    updating to current version: v0.1.40

--- conf.py
+++ conf.py
@@ -9,7 +9,7 @@
 project = 'morebs'
 copyright = '2023, Richard Pham'
 author = 'Richard Pham'
-release = '0.1.39'
+release = '0.1.40'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
