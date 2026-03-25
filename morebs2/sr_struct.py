00b539b2de59dc52394304c76fbbea870e87d2a5
Richard Pham <phamrichard45@gmail.com>
2026-03-24 22:03:24 -0400

    .

--- morebs2/sr_struct.py
+++ morebs2/sr_struct.py
@@ -3,6 +3,35 @@ file for simulation of recursive processes
 """
 from types import MethodType,FunctionType
 
+
+"""
+Simulates a recursive method. 
+
+Is comprised of n conditional statements, n > 0, and a possible NOT conditional. 
+Every conditional has 4 elements: 
+[0] boolean function : accept or reject parameters.
+    - null for NOT conditional.  
+[1] parameter delta function: modifies parameters. 
+    - optional; can be null. 
+[2] output function: maps parameters to an output value. 
+    - optional; can be null. 
+[3] termination function: outputs boolean for termination of recursive process on `parameters.
+
+The `pre_delta_function` P is optional. 
+
+----------------------------------------------------------------------------------------------
+
+Process is first set with starting `parameters`, via method<load_parameters>. 
+
+For every recursive call (method<__next__>): 
+- If P is not null, apply 
+    `parameters` = P(`parameters`). 
+- Iterate through the conditionals c_i in C: 
+    if c_i[0](`parameters`): 
+        Process `parameters` using that conditional. 
+        Quit call.
+- If no conditionals apply, then apply the NOT conditional. 
+"""
 class SimulatedRecursionNode:
 
     def __init__(self,pre_delta_function,conditionals,not_conditional): 
@@ -11,10 +40,10 @@ class SimulatedRecursionNode:
 
         assert len(conditionals) > 0 
         for x in conditionals: 
-            self.assert_proper_cond(x,False,False) 
+            self.assert_proper_cond(x,False) 
         self.conditionals = conditionals
 
-        self.assert_proper_cond(not_conditional,True,True)
+        self.assert_proper_cond(not_conditional,True)
         self.not_conditional = not_conditional
         self.parameters = None
         self.output_seq = [] 
@@ -23,8 +52,27 @@ class SimulatedRecursionNode:
         self.c = 0 
         return
 
-    def assert_proper_cond(self,x,is_not_cond:bool,non_permitted:bool):
-        if type(x) == type(None) and non_permitted: return 
+    def set_pre_delta_function(self,p): 
+        assert type(p) in {type(None),MethodType,FunctionType}
+        self.pre_delta_function = p 
+
+    def add_conditional(self,c): 
+        self.assert_proper_cond(c,False) 
+        self.conditionals.append(c) 
+        return 
+
+    def replace_conditional(self,c,index): 
+        assert 0 <= index < len(self.conditionals)
+        self.assert_proper_cond(c,False) 
+        self.conditionals[index] = c         
+        return
+
+    def set_NOT_conditional(self,c): 
+        self.assert_proper_cond(c,True) 
+        return
+
+    def assert_proper_cond(self,x,is_not_cond:bool):
+        if type(x) == type(None) and is_not_cond: return 
 
         assert type(x) == tuple  
         assert len(x) == 4 
@@ -68,7 +116,7 @@ class SimulatedRecursionNode:
         if q(*self.parameters): 
             self.one_proc(x) 
             return True 
-            
+
         return False 
 
     def one_proc(self,x): 
@@ -99,4 +147,5 @@ class SimulatedRecursionNode:
                 break 
 
         if not stat_: 
-            self.process_conditional_(self.not_conditional)
+            stat_ = self.process_conditional_(self.not_conditional)
+        return stat_ 
--- morebs2/tests/sr_struct_test.py
+++ morebs2/tests/sr_struct_test.py
@@ -1,6 +1,41 @@
 from morebs2.sr_struct import * 
 import unittest
 
+def SimulatedRecursionNode__sample_T(): 
+
+    def pre_delta_function(x0,x1,x2): 
+        x0 += 1 
+        x1 -= 1 
+        x2 += 1 
+        return [x0,x1,x2]
+
+    c0 = lambda x0,x1,x2: x0 + x1 + x2 > 100 
+    c1 = None 
+    c2 = lambda x0,x1,x2: x0 + x1 + x2 
+    c3 = lambda x0,x1,x2: (x0 + x1 + x2) % 131 == 0
+
+    C = (c0,c1,c2,c3)
+
+    c4 = lambda x0,x1,x2: (x0 + x1 + x2) % 78 == 0
+    C2 = (None,None,c2,c4)
+
+    S = SimulatedRecursionNode(pre_delta_function,[C],C2) 
+    return S 
+
+
+def SimulatedRecursionNode__sample_U(): 
+    S = SimulatedRecursionNode__sample_T() 
+
+    def pre_delta_function(x0,x1,x2): 
+        x0 *= 2 
+        x1 -= 117 
+        x2 += 1919
+        return [x0,x1,x2]
+
+    S.set_pre_delta_function(pre_delta_function)
+
+    return S 
+
 #########################################################
 
 '''
@@ -10,25 +45,9 @@ class TestSimulatedRecursionNode(unittest.TestCase):
 
     def test__SimulatedRecursionNode__next__case_1(self):#
 
-        def pre_delta_function(x0,x1,x2): 
-            x0 += 1 
-            x1 -= 1 
-            x2 += 1 
-            return [x0,x1,x2]
+        S = SimulatedRecursionNode__sample_T()
 
-        c0 = lambda x0,x1,x2: x0 + x1 + x2 > 100 
-        c1 = None 
-        c2 = lambda x0,x1,x2: x0 + x1 + x2 
-        c3 = lambda x0,x1,x2: (x0 + x1 + x2) % 131 == 0
-
-        C = (c0,c1,c2,c3)
-
-        c4 = lambda x0,x1,x2: (x0 + x1 + x2) % 78 == 0
-        C2 = (None,None,c2,c4)
-
-        S = SimulatedRecursionNode(pre_delta_function,[C],C2) 
         p = [23,-5,16] 
-
         S.load_parameters(p) 
 
         while not S.terminated: 
@@ -37,6 +56,20 @@ class TestSimulatedRecursionNode(unittest.TestCase):
         assert S.parameters == [67, -49, 60]
         assert len(S.output_seq) == 44, "got {}".format(len(S.output_seq))
         assert S.output_seq[-1] == 78 
+
+    def test__SimulatedRecursionNode__next__case_2(self):#
+
+        S = SimulatedRecursionNode__sample_U()
+
+        p = [23,-5,16] 
+        S.load_parameters(p) 
+
+        while not S.terminated: 
+            next(S) 
+
+        q = sum(S.parameters)
+        assert q == 444884701618183536292024147
+        assert q % 131 == 0 
 
 if __name__ == '__main__':
     unittest.main()
